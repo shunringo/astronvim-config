f890fb0d900d1867ba01a7e5981d61242e88a40f
Shunta Ichimura <shunringo223@gmail.com>
2024-01-03 22:51:58 +0900

    neo-git追加

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,19 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+    {
+    "TimUntersberger/neogit",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    dependencies = {
+      "sindrets/diffview.nvim",
+    },
+    config = function()
+      require("neogit").setup {
+        integrations = {
+          diffview = true,
+        },
+      }
+    end,
+  },
 }
